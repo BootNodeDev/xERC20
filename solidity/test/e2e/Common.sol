0743e16b73654665049c6e43b8242348cbb100ae
Lisandro Corbalan <lmcorbalan@gmail.com>
2024-08-12 20:17:59 -0300

    chore: add upgradable tests

--- solidity/test/e2e/Common.sol
+++ solidity/test/e2e/Common.sol
@@ -5,8 +5,10 @@ import {IERC20} from 'isolmate/interfaces/tokens/IERC20.sol';
 import {Test} from 'forge-std/Test.sol';
 import {XERC20} from '../../contracts/XERC20.sol';
 import {XERC20Lockbox} from '../../contracts/XERC20Lockbox.sol';
-
 import {XERC20Factory} from '../../contracts/XERC20Factory.sol';
+
+import {XERC20Upgradeable} from '../../contracts/XERC20Upgradeable.sol';
+import {XERC20UpgradeableFactory} from '../../contracts/XERC20UpgradeableFactory.sol';
 
 contract CommonE2EBase is Test {
   uint256 internal constant _FORK_BLOCK = 18_213_198;
@@ -38,6 +40,42 @@ contract CommonE2EBase is Test {
     address payable _lock = _xerc20Factory.deployLockbox(_token, address(_dai), false);
 
     _xerc20 = XERC20(_token);
+    _lockbox = XERC20Lockbox(_lock);
+    vm.stopPrank();
+  }
+}
+
+contract CommonE2EBaseUpgrade is Test {
+  uint256 internal constant _FORK_BLOCK = 18_213_198;
+  uint256 internal _userPrivateKey = 0x1234;
+  address internal _user = vm.addr(_userPrivateKey);
+  address internal _owner = vm.addr(200);
+  address internal _multicall = vm.addr(300);
+  address internal _testMinter = vm.addr(123_456_789);
+
+  IERC20 internal _dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+  XERC20Upgradeable internal _xerc20;
+  XERC20Lockbox internal _lockbox;
+  XERC20UpgradeableFactory internal _xerc20Factory;
+
+  function setUp() public virtual {
+    vm.createSelectFork(vm.rpcUrl(vm.envString('ETHEREUM_MAINNET_RPC')), _FORK_BLOCK);
+    uint256[] memory _minterLimits = new uint256[](1);
+    uint256[] memory _burnerLimits = new uint256[](1);
+    address[] memory _bridges = new address[](1);
+
+    _bridges[0] = _testMinter;
+    _minterLimits[0] = 100 ether;
+    _burnerLimits[0] = 50 ether;
+
+    vm.startPrank(_owner);
+    _xerc20Factory = new XERC20UpgradeableFactory();
+    (address _token,) = _xerc20Factory.deployXERC20Upgradeable(
+      _dai.name(), _dai.symbol(), _minterLimits, _burnerLimits, _bridges, 0, address(0)
+    );
+    address payable _lock = _xerc20Factory.deployLockbox(_token, address(_dai), false);
+
+    _xerc20 = XERC20Upgradeable(_token);
     _lockbox = XERC20Lockbox(_lock);
     vm.stopPrank();
   }
